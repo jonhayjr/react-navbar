9a707f75f169f2a70e1c86138feff9f0110dd592
Jon Hay <33227538+jonhayjr@users.noreply.github.com>
2022-05-03 07:58:41 -0700

    Made changes to Navbar styling and header component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,3 @@
-import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import {FaTimes, FaBars} from 'react-icons/fa';
 
@@ -10,26 +9,27 @@ const Navbar = () => {
   return (
     <>
         <div className='desktop-nav bg-black text-white p-7 flex justify-between items-center'>
-            <h1 className='text-2xl m-0 cursor-pointer font-bold tracking-wide'><Link to='/'>Jon Hay</Link></h1>
-            <nav className='hidden md:flex justify-between text-lg'>
-                <Link to='/' className='mr-10 hover:underline'>Home</Link>
-                <Link to='/about' className='mr-10 hover:underline'>About</Link>
-                <Link to='/services' className='mr-10 hover:underline'>Service</Link>
-                <Link to='/contact' className='hover:underline'>Contact</Link>
+            <h1 className='logo text-4xl m-0 cursor-pointer font-bold tracking-wide text-red-600'><Link to='/'>JH</Link></h1>
+            <nav className='hidden md:flex justify-between text-2xl'>
+                <Link to='/' className='mr-12 hover:underline hover:text-red-600'>Home</Link>
+                <Link to='/about' className='mr-12 hover:underline hover:text-red-600'>About</Link>
+                <Link to='/projects' className='mr-12 hover:underline hover:text-red-600'>Projects</Link>
+                <Link to='/contact' className='hover:underline hover:text-red-600'>Contact</Link>
             </nav>
-            <div className="mobile-hamburger md:hidden cursor-pointer z-10 hover:opacity-50" onClick={handleClick}>
+            <div className="mobile-hamburger md:hidden cursor-pointer z-10 hover:text-red-600" onClick={handleClick}>
                 {show ? <FaBars size={25}/> : <FaTimes size={25}/>}
             </div>
         </div>
-        {!show && <div className="mobile-menu bg-black text-white flex flex-col text-center text-3xl h-full absolute top-0 left-0 py-10 w-full -z-1">
-            <Link to='/' className='mb-10 hover:opacity-50'>Home</Link>
-            <hr className='w-2/3 mx-auto'/>
-            <Link to='/about' className='my-10 hover:opacity-50'>About</Link>
-            <hr className='w-2/3 mx-auto'/>
-            <Link to='/projects' className='my-10 hover:opacity-50'>Projects</Link>
-            <hr className='w-2/3 mx-auto'/>
-            <Link to='/contact' className="mt-10 hover:opacity-50">Contact</Link>
-        </div>}
+     
+           {!show && <div className='mobile-menu bg-black text-white flex flex-col text-center text-3xl absolute top-0 left-0 py-10 w-full h-full'>
+                <Link to='/' className='mb-10 hover:text-red-600'>Home</Link>
+                <hr className='w-2/3 mx-auto'/>
+                <Link to='/about' className='my-10 hover:text-red-600'>About</Link>
+                <hr className='w-2/3 mx-auto'/>
+                <Link to='/projects' className='my-10 hover:text-red-600'>Projects</Link>
+                <hr className='w-2/3 mx-auto'/>
+                <Link to='/contact' className="mt-10 hover:text-red-600">Contact</Link>
+            </div>}
     </>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,10 @@ const Home = () => {
   return (
       <>
         <div className='hero flex justify-center items-center'>
-            <div className='text-white text-center flex flex-col shadow-3xl bg-gray-800 p-10'>
-                <h2 className="text-4xl font-bold">My name is Jon</h2>
-                <p className="text-3xl font-bold mt-3">And I like to code</p>
-                <button className='mt-7 border-white border-8 p-4 block uppercase font-bold bg-transparent tracking-wide text-xl hover:bg-white hover:text-black'><Link to='/projects'>See my work</Link></button>
+            <div className='text-white flex flex-col text-center shadow-3xl bg-slate-800 p-10 w-8/12 mx-auto max-w-xl rounded'>
+                <h2 className="text-4xl font-bold">My name is <span className='text-red-600 text-5xl uppercase block'>Jon</span></h2>
+                <p className="text-3xl font-bold mt-3">And I like to <span className='text-red-600 text-5xl uppercase block'>code</span></p>
+                <button className='mt-7 border-white border-8 p-4 block uppercase font-bold bg-transparent tracking-wide text-xl hover:text-black hover:bg-white'><Link to='/projects'>See my work</Link></button>
             </div>
         </div>
       </>
